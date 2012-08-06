ac92b413bb740221f7ccf21c92543faef8ea033a
Seth Dowden <sethclarkdowden@gmail.com>
2012-08-06 13:53:21 -0700

    crated rails project and Posts db

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewBlog::Application.config.secret_token = '541e566658f5b78566a2e8985abddeb508a53b44db70bb33c9276de49dd4ca3823eabc542d2a0ca598c3268c1bd17297eec1efb82fec8cd6b4f86d06e8ccf6ba'
---
+++ db/migrate/20120806200004_change_post_string_to_text.rb
@@ -0,0 +1,9 @@
+class ChangePostStringToText < ActiveRecord::Migration
+  def up
+  	change_column :posts, :body, :text
+  end
+
+  def down
+  	change_column :posts, :body, :string
+  end
+end
