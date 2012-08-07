40334d895d0057090c09c5f2ed863c7db7fe820a
Seth Dowden <sethclarkdowden@gmail.com>
2012-08-06 20:04:54 -0700

    added curd vews and controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,18 @@ class PostsController < ApplicationController
 		@post = Post.find(params[:id])
 	end
 
+	def new
+		@post = Post.new
+	end
+
+	def edit
+		@post = Post.find(params[:id])	
+	end
+
+	def delete
+		@post = Post.destory(param[:id])
+
+		redirect_to posts_path(@post)
+	end
+
 end
