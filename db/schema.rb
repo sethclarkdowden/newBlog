976d61ce42db05aaa8e81fc22364441c1f1937d2
Seth Dowden <sethclarkdowden@gmail.com>
2012-08-06 21:51:28 -0700

    added author to posts db receving error olny on edit post

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120806200004) do
+ActiveRecord::Schema.define(:version => 20120807044025) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
     t.text     "body",       :limit => 255
     t.datetime "created_at",                :null => false
     t.datetime "updated_at",                :null => false
+    t.string   "author"
   end
 
 end
