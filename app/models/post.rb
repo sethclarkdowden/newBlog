f49aea8f961c4225b030b8f374c50f16dc1bb514
Seth Dowden <sethclarkdowden@gmail.com>
2012-08-08 20:24:38 -0700

    adding some css

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
    attr_accessible :title, :body, :author, :img
    has_many :comments
-   has_attached_file :img, :styles => { :medium => "600x400>", :thumb => "300x200>" }
+   has_attached_file :img, :styles => { :medium => "600x400>", :thumb => "200x200>" }
 end
